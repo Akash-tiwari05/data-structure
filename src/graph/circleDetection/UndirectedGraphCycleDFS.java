ad040482f6313abce29b19240c99061e822ef12d
Akash-tiwari05 <myselfakashtiwari05@gmail.com>
2026-06-15 18:52:02 +0530

    cyclic graph pattern

---
+++ src/graph/circleDetection/UndirectedGraphCycleDFS.java
@@ -0,0 +1,15 @@
+package graph.circleDetection;
+
+public class UndirectedGraphCycleDFS {
+
+    public static void main(String[] args) {
+        int V = 4, E = 4;
+        int[][] edges = {{0, 1}, {0, 2}, {1, 2}, {2, 3}};
+
+    }
+
+    public boolean isCycle(int V, int[][] edges) {
+        // Code here
+
+    }
+}
