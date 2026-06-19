bebac29a1f66401aec50f224acc4b41c4d082667
Akash-tiwari05 <myselfakashtiwari05@gmail.com>
2026-06-19 22:22:09 +0530

    cyclic graph pattern

--- src/graph/circleDetection/UndirectedGraphCycleDFS.java
+++ src/graph/circleDetection/UndirectedGraphCycleDFS.java
@@ -1,15 +1,51 @@
 package graph.circleDetection;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class UndirectedGraphCycleDFS {
 
     public static void main(String[] args) {
         int V = 4, E = 4;
-        int[][] edges = {{0, 1}, {0, 2}, {1, 2}, {2, 3}};
+        int[][] edges = {{0, 1}, {1, 2}, {2, 3}};
+        System.out.println(isCycle(V, edges));
 
     }
 
-    public boolean isCycle(int V, int[][] edges) {
+    public static boolean isCycle(int V, int[][] edges) {
         // Code here
+        ArrayList<Integer>[] adj = new ArrayList[V];
 
+        for (int i = 0; i < V; i++) {
+            adj[i] = new ArrayList<>();
+        }
+
+        for (int[] edge: edges) {
+            adj[edge[0]].add(edge[1]);
+            adj[edge[1]].add(edge[0]);
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[V];
+        visited[0] = true;
+        q.offer(edges[0][0]);
+        while (!q.isEmpty()){
+
+            int curr = q.poll();
+
+            for(int neighbor: edges[curr]){
+                if(!visited[neighbor]){
+                    visited[neighbor] = true;
+                    q.offer(neighbor);
+                }else{
+                    return true;
+                }
+
+            }
+        }
+        return false;
     }
 }
